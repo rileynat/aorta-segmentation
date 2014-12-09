bed1705b2812e714d2221719f2bbd0d582dc90e8
kenzshelley <kenzshelley@gmail.com>
2014-12-09 03:40:46 -0500

    Updated to use app metric; cleaned things up in general

--- cnn/test_cnn.m
+++ cnn/test_cnn.m
@@ -1,10 +1,23 @@
-function [ accuracy ] = test_cnn( test_data, test_labels, weights, filterInfo )
+function [ ap_test, acc_test ] = test_cnn( test_data, test_labels, weights, filterInfo )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-    [~, theta] = roll_params(0, weights);
-    [accuracy, ~] = gradient_cnn(theta, test_data, test_labels, filterInfo, 0);
+    %[~, theta] = roll_params(0, weights);
+    %[accuracy, ~] = gradient_cnn(theta, test_data, test_labels, filterInfo, 0);
     
+    disp('Testing...');
     
+    % FeedForward
+    yhat_pred = FeedForwardCNN(test_data, weights, filterInfo);
+    
+    ap_test = zeros(size(test_data, 3), 1);
+    acc_test = zeros(size(test_data,3), 1);
+    for i = 1:size(test_data, 3)
+       yhat = yhat_pred(:,:,i);
+       yc = test_labels(:,:,i);
+       [~, ~, ap] = compute_ap(yhat(:), yc(:));
+       ap_test(i) =  ap;
+       acc_test(i) = 1-mean(yc(:) ~= (yhat(:) > 0.5));
+    end
 
 end
 
--- demo_nate_model.m
+++ demo_nate_model.m
@@ -1,3 +1,4 @@
+function [] = demo_nate_model(weights)
 
 close all;
 clc;
@@ -13,8 +14,9 @@ end
 train_data_x = [];
 train_data_y = [];
 
+data_set = 4;
 
-for i=4
+for i=data_set
     filename = sprintf('normalized_data/train/%02d.mat', i);
     cModel = load(filename);
     x_input = double(cModel.cModel.x);
@@ -26,7 +28,7 @@ end
 val_data_x = [];
 val_data_y = [];
 
-for i=4
+for i=data_set
     filename = sprintf('normalized_data/val/%02d.mat', i);
     cModel = load(filename);
     x_input = double(cModel.cModel.x);
@@ -38,7 +40,7 @@ end
 test_data_x = [];
 test_data_y = [];
 
-for i=4
+for i=data_set
     filename = sprintf('normalized_data/test/%02d.mat', i);
     cModel = load(filename);
     x_input = double(cModel.cModel.x);
@@ -49,7 +51,7 @@ end
 
 filterInfo = struct;
 filterInfo.numFilters1 = 30;
-filterInfo.filterSize1 = 8;
+filterInfo.filterSize1 = 5;
 filterInfo.numFilters2 = 30;
 filterInfo.filterSize2 = 16;
 filterInfo.numFilters3 = filterInfo.numFilters2;
@@ -60,14 +62,24 @@ train_data_x = fit_HUscale(train_data_x);
 
 addpath('cnn/');
 
-[weights] = train_cnn(train_data_x(:,:,1:1), train_data_y(:,:,1:1), filterInfo); 
+if ~exist('weights', 'var')
+    disp('Training...');
+    [weights] = train_cnn(train_data_x(:,:,1:1), train_data_y(:,:,1:1), filterInfo); 
+end
 
-save(strcat('cnn/weights/weights_', date, '.mat'), 'weights');
+[valAP, valAcc] = validate_cnn(val_data_x(:,:,:), val_data_y(:,:,:), weights, filterInfo);
 
-[validAcc] = validate_cnn(val_data_x(:,:,:), val_data_y(:,:,:), weights, filterInfo);
+[testAP, testAcc] = test_cnn(test_data_x(:,:,:), test_data_y(:,:,:), weights, filterInfo);
 
-disp(validAcc);
+fprintf('AP: val = %g (std %g), test = %g (std %g))\n', ...
+        mean(valAP), std(valAP), mean(testAP), std(testAP));
+fprintf('ACC: val = %g (std %g), test = %g (std %g))\n', ...
+        mean(acc_val), std(acc_val), mean(acc_test), std(acc_test));
+  
+% Save all useful info in a directory named by date/time
+info = clock;
+mat_name = sprintf('results/%d-%d-%d-%d:%d-set-%d', clock(1), clock(2), clock(3), clock(4), clock(5), data_set);
+save(mat_name, 'weights', 'valAP', 'valAcc', 'testAP', 'testAcc');
 
-[testAcc] = test_cnn(test_data_x(:,:,:), test_data_y(:,:,:), weights, filterInfo);
+end
 
-disp(testAcc);
